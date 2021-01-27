242014f8a48ea990a9604a06b3da6460cc454c24
Johnny-smiles <70188711+Johnny-smiles@users.noreply.github.com>
2021-01-26 21:07:39 -0600

    20.3.4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+// linking react
 import React from 'react';
+// linking nav component
 import Nav from './componets/Nav';
+// linking about component
 import About from './componets/About';
+// linking gallery component
+import Gallery from "./components/Gallery";
+
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
